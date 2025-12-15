8a698bca0a06bbbb090028d70d1433fda78d372a
yf91 <yackfou09@gmail.com>
2025-12-15 14:50:47 +0100

    Add Prisma Accelerate

--- admin/lib/prisma.ts
+++ admin/lib/prisma.ts
@@ -1,20 +1,18 @@
 import { PrismaClient } from "@/prisma/generated/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+  prisma: PrismaClient & ReturnType<typeof withAccelerate>;
 };
 
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    adapter,
-  });
+    accelerateUrl: process.env.DATABASE_URL!,
+  }).$extends(withAccelerate());
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export { prisma };
